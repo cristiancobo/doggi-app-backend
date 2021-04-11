bdcda4422de714819aff0350d254f5765c722a63
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-04-11 17:14:01 -0500

    creation and implementation color entity

---
+++ src/main/java/com/app/doggi/model/Color.java
@@ -0,0 +1,38 @@
+package com.app.doggi.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Class representing a dog's color
+ * @version 1.0
+ */
+@Entity
+@Table(name = "COLOR")
+public class Color {
+    //Attributes
+    @Id
+    @Column(name = "ID", nullable = false)
+    private Long id;
+
+    @Column(name = "NAME", nullable = false)
+    private String name;
+
+    //Access methods
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
