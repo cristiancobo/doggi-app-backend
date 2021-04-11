6509f1d2d4dbc5e957d11ebc71fe23d4ceb91f2c
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-04-11 18:46:26 -0500

    access methods id attribute deleted from dog breed in dto

--- src/main/java/com/app/doggi/stdin/DogBreedStdInDto.java
+++ src/main/java/com/app/doggi/stdin/DogBreedStdInDto.java
@@ -17,14 +17,6 @@ public class DogBreedStdInDto {
 
     //Acces methods
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getName() {
         return name;
     }
