874ad852bbf843514bdf848344ce65468d04bcaa
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-04-11 19:38:06 -0500

    dog breed interface service was updated - problems with mappers solved

--- src/main/java/com/app/doggi/interfaces/service/IDogBreedService.java
+++ src/main/java/com/app/doggi/interfaces/service/IDogBreedService.java
@@ -1,12 +1,18 @@
 package com.app.doggi.interfaces.service;
 
 
+import com.app.doggi.dtos.stdin.DogBreedStdInDto;
 import com.app.doggi.model.DogBreed;
 import org.springframework.stereotype.Service;
 
+/**
+ * Interface representing dog breed interface service.
+ * @version 1.0
+ */
 @Service
 public interface IDogBreedService {
-
-
-
+    public DogBreed save(DogBreedStdInDto dogBreedStdInDto);
+    public DogBreed update(Long id, DogBreedStdInDto dogBreedStdInDto);
+    public DogBreed delete(Long id);
+    public DogBreed findById(Long id);
 }
