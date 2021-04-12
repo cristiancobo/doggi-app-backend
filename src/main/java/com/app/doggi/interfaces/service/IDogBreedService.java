cd002fe14485aa7671d86fcfd86a75fb81ef4827
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-04-11 21:18:49 -0500

    git commit -m

--- src/main/java/com/app/doggi/interfaces/service/IDogBreedService.java
+++ src/main/java/com/app/doggi/interfaces/service/IDogBreedService.java
@@ -2,8 +2,11 @@ package com.app.doggi.interfaces.service;
 
 
 import com.app.doggi.dtos.stdin.DogBreedStdInDto;
+import com.app.doggi.dtos.stdout.DogBreedStdOutDto;
 import com.app.doggi.model.DogBreed;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Interface representing dog breed interface service.
@@ -15,4 +18,5 @@ public interface IDogBreedService {
     public DogBreed update(Long id, DogBreedStdInDto dogBreedStdInDto);
     public DogBreed delete(Long id);
     public DogBreed findById(Long id);
+    public List<DogBreedStdOutDto> findAll();
 }
