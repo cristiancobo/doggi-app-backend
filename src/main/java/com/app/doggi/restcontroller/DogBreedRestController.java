c46de73879c0f0010c1acc424257fbd227a292d7
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-04-11 22:46:25 -0500

    implementation save dog breed

--- src/main/java/com/app/doggi/restcontroller/DogBreedRestController.java
+++ src/main/java/com/app/doggi/restcontroller/DogBreedRestController.java
@@ -1,5 +1,13 @@
 package com.app.doggi.restcontroller;
 
+import com.app.doggi.dtos.stdin.DogBreedStdInDto;
+import com.app.doggi.dtos.stdout.DogBreedStdOutDto;
+import com.app.doggi.interfaces.service.IDogBreedService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("dogbreed")
 public class DogBreedRestController {
+
+    private IDogBreedService iDogBreedService;
+
+    @Autowired
+    public DogBreedRestController(IDogBreedService iDogBreedService){
+        this.iDogBreedService = iDogBreedService;
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<DogBreedStdOutDto> saveDogBreed(@RequestBody DogBreedStdInDto dogBreedStdInDto){
+        DogBreedStdOutDto dogBreedStdOutDto = iDogBreedService.save(dogBreedStdInDto);
+        return new ResponseEntity<DogBreedStdOutDto>(dogBreedStdOutDto, HttpStatus.OK);
+    }
 
 
 }
