90aabfcccc0186aba462936005d652e32f0179ce
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-04-11 22:40:22 -0500

    creation dog breed entity rest controller

---
+++ src/main/java/com/app/doggi/restcontroller/DogBreedRestController.java
@@ -0,0 +1,15 @@
+package com.app.doggi.restcontroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Class representing a dog breed's controller
+ * @version 1.0
+ */
+@RestController
+@RequestMapping("dogbreed")
+public class DogBreedRestController {
+
+
+}
