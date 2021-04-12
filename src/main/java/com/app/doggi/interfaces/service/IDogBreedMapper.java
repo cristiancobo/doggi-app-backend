83a77394fb12389bc42fdf6169d0b3f9ea160134
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-04-11 19:30:08 -0500

    dof breed interface and implementation mapper were updated

---
+++ src/main/java/com/app/doggi/interfaces/service/IDogBreedMapper.java
@@ -0,0 +1,16 @@
+package com.app.doggi.interfaces.service;
+
+import com.app.doggi.dtos.stdin.DogBreedStdInDto;
+import com.app.doggi.dtos.stdout.DogBreedStdOutDto;
+import com.app.doggi.model.DogBreed;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface IDogBreedMapper {
+    IDogBreedMapper INTANCE = Mappers.getMapper(IDogBreedMapper.class);
+    DogBreedStdOutDto asDogBredToDogBreedStdOutDto(DogBreed dogBreed);
+    DogBreed asDogBredToDogBreedStdInDtoToDogBreed(DogBreedStdInDto dogBreedStdInDto);
+
+
+}
