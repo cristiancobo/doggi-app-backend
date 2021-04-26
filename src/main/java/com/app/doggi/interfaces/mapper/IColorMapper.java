36463caf0e1f58b0d7470e9314303d1d5e12bb2e
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-04-26 11:25:05 -0500

    mapper annotation added to color mapper

--- src/main/java/com/app/doggi/interfaces/mapper/IColorMapper.java
+++ src/main/java/com/app/doggi/interfaces/mapper/IColorMapper.java
@@ -4,9 +4,10 @@ package com.app.doggi.interfaces.mapper;
 
 import com.app.doggi.dtos.stdout.ColorStdOutDto;
 import com.app.doggi.model.Color;
+import org.mapstruct.Mapper;
 
 import java.util.List;
-
+@Mapper
 public interface IColorMapper {
 
     ColorStdOutDto asColorToColorStdOutDto(Color color);
