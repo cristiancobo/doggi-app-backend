232bd00f0280610e7d8970c1df8eff3550443512
cristiancoboramirez <cristian.cobo1@u.icesi.edu.co>
2021-04-26 11:24:39 -0500

    mapper annotation added to nature mapper

---
+++ src/main/java/com/app/doggi/implementation/service/ColorServiceImpl.java
@@ -0,0 +1,2 @@
+package com.app.doggi.implementation.service;public class ColorServiceImpl {
+}
--- src/main/java/com/app/doggi/interfaces/mapper/INatureMapper.java
+++ src/main/java/com/app/doggi/interfaces/mapper/INatureMapper.java
@@ -2,9 +2,10 @@ package com.app.doggi.interfaces.mapper;
 
 import com.app.doggi.dtos.stdout.NatureStdOutDto;
 import com.app.doggi.model.Nature;
+import org.mapstruct.Mapper;
 
 import java.util.List;
-
+@Mapper
 public interface INatureMapper {
 
     NatureStdOutDto asNatureToNatureStdOutDto(Nature nature);
